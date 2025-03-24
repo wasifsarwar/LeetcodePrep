14649acdc0811c8622b399c9117ba6648b899273
Wasif Siddique <wsiddique@axon.com>
2025-03-23 19:57:34 -0700

    encoding and topKfrequentElements

---
+++ Neetcode/Arrays&Hashing/EncodingAndDecoding.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+public class EncodingAndDecoding {
+
+ 
+    public static void main(String[] args) {
+
+    }
+
+    public static String encode(List<String> strs) {
+
+    }
+
+    public static List<String> decode(String str) {
+
+    }
+
+}
---
+++ Neetcode/Arrays&Hashing/TopKFrequentElements.java
@@ -0,0 +1,10 @@
+public class TopKFrequentElements {
+    
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] topKFrequentElements(int[] nums, int k) {
+        
+    }
+}
